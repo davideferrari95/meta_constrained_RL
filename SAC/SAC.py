57ac36d547fdafe526104ad223021d22a097ecf6
davideferrari95 <davideferr@unimore.it>
2022-12-03 22:10:55 +0100

    Fixed Cost 'beta' Penalty Factor

--- SAC/SAC.py
+++ SAC/SAC.py
@@ -29,16 +29,17 @@ class SAC(LightningModule):
     # epsilon:              Epsilon = Prob. to Take a Random Action
     # samples_per_epoch:    How many Observation in a single Epoch
     # tau:                  How fast we apply the Polyak Average Update
-    # alpha:                Importance of the Log Probability of the Selected Action
+    # alpha:                Importance of the Log Probability of the Selected Action -> Entropy Scale Factor
+    # beta:                 Importance of the Cost -> Cost Scale Factor
 
-    def __init__(self, env_name, folder_name, capacity=100_000, batch_size=512, lr=1e-3,
+    def __init__(self, env_name, capacity=100_000, batch_size=512, lr=1e-3,
                  hidden_size=256, gamma=0.99, loss_function=F.smooth_l1_loss, optim=AdamW, 
-                 epsilon=0.05, samples_per_epoch=10_000, tau=0.05, alpha=0.02):
+                 epsilon=0.05, samples_per_epoch=10_000, tau=0.05, alpha=0.02, beta=0.02):
 
         super().__init__()
 
         # Create Environment
-        self.env = create_environment(env_name, folder=folder_name)
+        self.env = create_environment(env_name)
 
         # Action, Observation Dimension 
         obs_size = self.env.observation_space.shape[0]
@@ -52,11 +53,15 @@ class SAC(LightningModule):
         self.q_net2 = DQN(hidden_size, obs_size, action_dims).to(DEVICE)
         self.policy = GradientPolicy(hidden_size, obs_size, action_dims, max_action).to(DEVICE)
         self.buffer = ReplayBuffer(capacity)
+        
+        # Create Cost NN
+        self.q_net_cost = DQN(hidden_size, obs_size, action_dims).to(DEVICE)
 
         # Create Target Networks
         self.target_q_net1 = copy.deepcopy(self.q_net1)
         self.target_q_net2 = copy.deepcopy(self.q_net2)
         self.target_policy = copy.deepcopy(self.policy)
+        self.target_q_net_cost = copy.deepcopy(self.q_net_cost)
 
         # Save Hyperparameters in Internal Properties that we can Reference in our Code
         self.save_hyperparameters()
@@ -101,9 +106,8 @@ class SAC(LightningModule):
             # Get Cumulative Cost from the Info Dict 
             cost = info.get('cost', 0)
 
-            # Update and Log Episode Cost
+            # Update Episode Cost
             episode_cost += cost
-            if policy: self.log('episode/Cost', episode_cost)
             
             # Save Experience (with cost) in Replay Buffer
             exp = (obs, action, reward, cost, done, next_obs)
@@ -111,6 +115,9 @@ class SAC(LightningModule):
         
             # Update State
             obs = next_obs
+        
+        # Log Episode Cost
+        if policy: self.log('episode/Cost', episode_cost)
     
     def forward(self, x):
         
@@ -119,9 +126,8 @@ class SAC(LightningModule):
     
     def configure_optimizers(self):
         
-        # TODO: Add Optimizer for Q-Cost
-        # Create an Iterator with the Parameters of the Critic Q-Networks
-        q_net_params = itertools.chain(self.q_net1.parameters(), self.q_net2.parameters())
+        # Create an Iterator with the Parameters of the Critic Q-Networks and the Q-Cost
+        q_net_params = itertools.chain(self.q_net1.parameters(), self.q_net2.parameters(), self.q_net_cost.parameters())
         
         # We need 2 Separate Optimizers as we have 2 Neural Networks
         q_net_optimizer  = self.hparams.optim(q_net_params,  lr=self.hparams.lr)
@@ -155,6 +161,9 @@ class SAC(LightningModule):
             action_values1 = self.q_net1(states, actions)
             action_values2 = self.q_net2(states, actions)
             
+            # Compute the Cost
+            cost_values = self.q_net_cost(states, actions)
+            
             # Compute Next Action and Log Probabilities
             target_actions, target_log_probs = self.target_policy(next_states)
             
@@ -164,16 +173,24 @@ class SAC(LightningModule):
                 self.target_q_net2(next_states, target_actions)
             )
             
-            # When episode is over (Done=True) we don't expect any new reward
+            # Compute Next Cost Values
+            next_cost_values = self.target_q_net_cost(next_states, target_actions)
+
+            # When episode is over (Done=True) we don't expect any new reward / cost -> (1-done)
             next_action_values[dones] = 0.0
+            next_cost_values[dones] = 0.0
             
             # Construct the Target, Adjusting the Value with α * log(π) 
             expected_action_values = rewards + self.hparams.gamma * (next_action_values - self.hparams.alpha * target_log_probs)
             
+            # Construct Cost Target
+            expected_cost_values = costs + self.hparams.gamma * (next_cost_values)
+            
             # Compute the Loss Function
             q_loss1 = self.hparams.loss_function(action_values1, expected_action_values)
             q_loss2 = self.hparams.loss_function(action_values2, expected_action_values)
-            total_loss = q_loss1 + q_loss2
+            q_cost_loss = self.hparams.loss_function(cost_values, expected_cost_values)
+            total_loss = q_loss1 + q_loss2 + q_cost_loss
             self.log('episode/Q-Loss', total_loss)
 
             return total_loss
@@ -189,9 +206,12 @@ class SAC(LightningModule):
                 self.q_net1(states, actions),
                 self.q_net2(states, actions)
             )
+            
+            # Compute the Cost
+            cost_values = self.q_net_cost(states, actions)
 
-            # Compute the Policy Loss
-            policy_loss = (self.hparams.alpha * log_probs - action_values).mean()
+            # Compute the Policy Loss (α * Entropy + β * Cost)
+            policy_loss = (self.hparams.alpha * log_probs - action_values + self.hparams.beta * cost_values).mean()
             self.log('episode/Policy Loss', policy_loss)
 
             return policy_loss
@@ -204,6 +224,7 @@ class SAC(LightningModule):
         # Polyak Average Update of the Networks
         polyak_average(self.q_net1, self.target_q_net1, tau=self.hparams.tau)
         polyak_average(self.q_net2, self.target_q_net2, tau=self.hparams.tau)
+        polyak_average(self.q_net_cost, self.target_q_net_cost, tau=self.hparams.tau)
         polyak_average(self.policy, self.target_policy, tau=self.hparams.tau)
         
         # Log Episode Return
