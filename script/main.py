ee7cebe4cdbb2f245f6a64d07e4357a7a21b96b3
davideferrari95 <davideferr@unimore.it>
2023-05-04 21:16:19 +0200

    Chenged Early Stopping Function

--- script/main.py
+++ script/main.py
@@ -62,7 +62,7 @@ def main(cfg: Params):
     # callbacks.append(ModelCheckpoint())
 
     # Optional Callbacks
-    if TP.early_stopping: callbacks.append(EarlyStopping(monitor='kl_divergence', mode='min', patience=TP.patience, verbose=True, check_on_train_epoch_end=True))
+    if TP.early_stopping: callbacks.append(EarlyStopping(monitor='avg_ep_reward', mode='max', patience=TP.patience, verbose=True, check_on_train_epoch_end=True))
 
     # Test Callback
     if EP.test_environment: callbacks.append(TestCallback())
@@ -75,11 +75,9 @@ def main(cfg: Params):
     trainer = Trainer(
 
         # Devices
-        # devices = 'cpu',
-        accelerator = 'cpu',
-        # devices = AUTO,
-        # accelerator = AUTO,
-        # strategy = AUTO,
+        devices = AUTO,
+        accelerator = AUTO,
+        strategy = AUTO,
         precision = TP.precision,
 
         # Hyperparameters
