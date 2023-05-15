4a4f18f3e4665f58c33f43c70d813e44637ffa30
davideferrari95 <davideferr@unimore.it>
2023-05-15 12:04:53 +0200

    Implemented AdamBA SC Algorithm

--- script/utils/AdamBA.py
+++ script/utils/AdamBA.py
@@ -1,15 +1,18 @@
 # Import Project Folder
 from utils.Utils import FOLDER
 
-import os, copy, time, json
+import os, copy, time, json, gym
 import numpy as np
-import gym
+from typing import Optional
 
 # Import QP Utilities
 from scipy import sparse
 import osqp
 
-def AdamBA(obs, act, env, threshold, dt_ratio=1.0, ctrlrange=10.0):
+def AdamBA(obs, act, env:gym.Env, threshold:float, dt_ratio:float=1.0, ctrlrange:float=10.0):
+
+    # TODO: type for obs, act
+    print(f'Type | obs: {type(obs)} | act: {type(act)}')
 
     # Reshape Action
     act, action_space_num = np.clip(act, -ctrlrange, ctrlrange), 2
@@ -60,7 +63,7 @@ def AdamBA(obs, act, env, threshold, dt_ratio=1.0, ctrlrange=10.0):
 
             # Update NP_vec
             NP_vec_tmp_i, NP_vec_dir_tmp_i = NP_vec_tmp[v], NP_vec_dir_tmp[v]
-            eta, decrease_flag = bound, 0
+            eta, decrease_flag = bound, False
 
             while eta >= bound:
 
@@ -77,32 +80,33 @@ def AdamBA(obs, act, env, threshold, dt_ratio=1.0, ctrlrange=10.0):
                 # AdamBA Procedure
 
                 # Outreach
-                if flag == 1 and decrease_flag == 0:
+                if flag == True and decrease_flag == False:
 
                     NP_vec_tmp_i = NP_vec_tmp_i + eta * NP_vec_dir_tmp_i
                     eta = eta * 2
                     continue
 
                 # Monitor for 1st Reaching Out Boundary
-                if flag == 0 and decrease_flag == 0:
+                if flag == False and decrease_flag == False:
 
-                    decrease_flag = 1
+                    decrease_flag = True
                     eta = eta * 0.25
                     continue
 
                 # Decrease Eta
-                if flag == 1 and decrease_flag == 1:
+                if flag == True and decrease_flag == True:
 
                     NP_vec_tmp_i = NP_vec_tmp_i + eta * NP_vec_dir_tmp_i
                     eta = eta * 0.5
                     continue
 
-                if flag == 0 and decrease_flag == 1:
+                if flag == False and decrease_flag == True:
 
                     NP_vec_tmp_i = NP_vec_tmp_i - eta * NP_vec_dir_tmp_i
                     eta = eta * 0.5
                     continue
 
+            # Update `NP_vec`
             NP_vec_tmp[v] = NP_vec_tmp_i
 
         NP_vec_tmp_new = []
@@ -133,8 +137,8 @@ def AdamBA(obs, act, env, threshold, dt_ratio=1.0, ctrlrange=10.0):
 
     # AdamBA Status
     if valid == 2: valid_adamba = "adamba success"
-    elif valid == 0 and yes==100: valid_adamba = "itself satisfy"
-    elif valid == 0 and out==100: valid_adamba = "all out"
+    elif valid == 0 and yes == 100: valid_adamba = "itself satisfy"
+    elif valid == 0 and out == 100: valid_adamba = "all out"
     elif valid == 1: valid_adamba = "one valid"
     else: valid_adamba = "exception"
 
@@ -148,16 +152,186 @@ def AdamBA(obs, act, env, threshold, dt_ratio=1.0, ctrlrange=10.0):
         b = threshold * (x1 - x2) / (y2 * x1 - y1 * x2)
         A = [a, b]
 
+        # Return the Safe Action
         return [A, threshold], valid_adamba
 
     # Return None
     else: return [None, None], valid_adamba
 
-def AdamBA_SC():
+def AdamBA_SC(obs, act, env:gym.Env, threshold:float=0, dt_ratio:float=1.0, ctrlrange:float=10.0, margin:float=0.4,
+              adaptive_k:float=3, adaptive_n:float=1, adaptive_sigma:float=0.04, trigger_by_pre_execute:bool=False,
+              pre_execute_coef:float=0.0, vec_num:Optional[int]=None, max_trial_num:int=1):
 
-    pass
+    # TODO: type for obs, act
+    print(f'Type | obs: {type(obs)} | act: {type(act)}')
 
-def store_heatmap(env:gym.Env, cnt_store_heatmap_trigger, trigger_by_pre_execute, safe_index_now, threshold, n, k, sigma, pre_execute):
+    # Reshape Action
+    act, action_space_num = np.clip(act, -ctrlrange, ctrlrange), env.action_space.shape[0]
+    action = np.array(act).reshape(-1, action_space_num)
+    limits= [[-ctrlrange, ctrlrange]] * action_space_num
+
+    # AdamBA Parameters
+    dt_adamba = env.model.opt.timestep * env.frameskip_binom_n * dt_ratio
+    loc, scale, bound = 0, 0.1, 0.0001
+
+    # Check `dt_ratio`
+    assert dt_ratio == 1
+
+    # Generate Direction
+    NP_vec_dir = []
+    NP_vec = []
+
+    # Generate `vec_num`
+    if action_space_num ==2: vec_num = 10 if vec_num == None else vec_num
+    elif action_space_num == 12: vec_num = 20 if vec_num == None else vec_num
+    else: raise NotImplementedError
+
+    # Num of Actions Input, Default = 1
+    for t in range(0, action.shape[0]):
+
+        if action_space_num == 2:
+
+            # Initialize Arrays
+            vec_set, vec_dir_set = [], []
+
+            for m in range(0, vec_num):
+
+                # Compute Action Vectors
+                theta_m = m * (2 * np.pi / vec_num)
+                vec_dir = np.array([np.sin(theta_m), np.cos(theta_m)]) / 2
+                vec_dir_set.append(vec_dir)
+                vec_set.append(action[t])
+
+            # Append Action Vectors
+            NP_vec_dir.append(vec_dir_set)
+            NP_vec.append(vec_set)
+
+        else:
+
+            # Compute Action Vectors
+            vec_dir_set = np.random.normal(loc=loc, scale=scale, size=[vec_num, action_space_num])
+            vec_set = [action[t]] * vec_num
+
+            # Append Action Vectors
+            NP_vec_dir.append(vec_dir_set)
+            NP_vec.append(vec_set)
+
+    # Record how many Boundary Points Found
+    valid, cnt, out, yes = 0, 0, 0, 0, 0
+
+    for n in range(0, action.shape[0]):
+
+        # Init Variables
+        trial_num, at_least_1 = 0, False
+
+        while trial_num < max_trial_num and not at_least_1:
+
+            # Increase Counter
+            trial_num, at_least_1 = trial_num + 1, False
+            NP_vec_tmp = copy.deepcopy(NP_vec[n])
+
+            # Compute NP_vec_dir
+            if trial_num == 1: NP_vec_dir_tmp = NP_vec_dir[n]
+            else: NP_vec_dir_tmp = np.random.normal(loc=loc, scale=scale, size=[vec_num, action_space_num])
+
+            for v in range(0, vec_num):
+
+                # Update NP_vec
+                NP_vec_tmp_i, NP_vec_dir_tmp_i = NP_vec_tmp[v], NP_vec_dir_tmp[v]
+                eta, decrease_flag = bound, False
+
+                while True:
+
+                    # Check Environment Safety
+                    flag, env = check_unsafe(obs, NP_vec_tmp_i, dt_ratio, dt_adamba, env, threshold, margin,
+                                             adaptive_k, adaptive_n, adaptive_sigma, trigger_by_pre_execute, pre_execute_coef)
+
+                    # Check Safety-Gym Env out-of-bound
+                    if out_of_bound(limits, NP_vec_tmp_i, sc=True): break
+
+                    # Check Boundaries
+                    if eta <= bound and flag == False:
+
+                        at_least_1 = True
+                        break
+
+                    # AdamBA SC Procedure
+
+                    # Outreach
+                    if flag == True and decrease_flag == False:
+
+                        NP_vec_tmp_i = NP_vec_tmp_i + eta * NP_vec_dir_tmp_i
+                        eta = eta * 2
+                        continue
+
+                    # Monitor for 1st Reaching Out Boundary
+                    if flag == False and decrease_flag == False:
+
+                        decrease_flag = True
+                        eta = eta * 0.25
+                        continue
+
+                    # Decrease Eta
+                    if flag == True and decrease_flag == True:
+
+                        NP_vec_tmp_i = NP_vec_tmp_i + eta * NP_vec_dir_tmp_i
+                        eta = eta * 0.5
+                        continue
+
+                    if flag == False and decrease_flag == True:
+
+                        NP_vec_tmp_i = NP_vec_tmp_i - eta * NP_vec_dir_tmp_i
+                        eta = eta * 0.5
+                        continue
+
+                # Update `NP_vec`
+                NP_vec_tmp[v] = NP_vec_tmp_i
+
+        NP_vec_tmp_new = []
+
+        for v_num in range(0, len(NP_vec_tmp)):
+
+            # Increase Counter
+            cnt += 1
+
+            if out_of_bound(limits, NP_vec_tmp[v_num], sc=True):
+
+                out += 1
+                continue
+
+            if NP_vec_tmp[v_num][0] == act[0][0] and NP_vec_tmp[v_num][1] == act[0][1]:
+
+                yes += 1
+                continue
+
+            valid += 1
+            NP_vec_tmp_new.append(NP_vec_tmp[v_num])
+
+        NP_vec[n] = NP_vec_tmp_new
+
+    NP_vec_tmp = NP_vec[0]
+
+    # AdamBA SC Status
+    if valid > 0: valid_adamba_sc = "adamba_sc success"
+    elif valid == 0 and yes == vec_num: valid_adamba_sc = "itself satisfy"
+    elif valid == 0 and out==vec_num: valid_adamba_sc = "all out"
+    else: valid_adamba_sc = "exception"
+
+    # At Least 1 Sampled Action Satisfying the Safety Index
+    if len(NP_vec_tmp) > 0:
+
+        # Compute the AdamBA SC Actions
+        norm_list = np.linalg.norm(NP_vec_tmp, axis=1)
+        optimal_action_index = np.where(norm_list == np.amin(norm_list))[0][0]
+
+        # Return the Safe Action
+        return NP_vec_tmp[optimal_action_index], valid_adamba_sc, env, NP_vec_tmp
+
+    # Return None
+    else: return None, valid_adamba_sc, env, None
+
+def store_heatmap(env:gym.Env, cnt_store_heatmap_trigger:int, trigger_by_pre_execute:bool, safe_index_now:float,
+                  threshold:float, n:float, k:float, sigma:float, pre_execute:bool):
 
     # HeatMap Logger
     heat_map_logger_trigger = dict()
@@ -314,7 +488,7 @@ def quadratic_programming(H, f, A=None, b=None, initvals=None, verbose=False):
     # Return Results
     return results.x, results.info.status
 
-def check_unsafe(s, point, dt_ratio, dt_adamba, env: gym.Env, threshold):
+def check_unsafe(s, point, dt_ratio:float, dt_adamba:float, env:gym.Env, threshold:float):
 
     # Create Action
     action = [point[0], point[1]]
@@ -342,9 +516,9 @@ def check_unsafe(s, point, dt_ratio, dt_adamba, env: gym.Env, threshold):
     # Projection d_phi = cost_future - cost_now
     d_phi = projection_cost_future - projection_cost_now
 
-    # Safe = 0 | Unsafe = 1
-    if d_phi <= threshold * dt_adamba: flag = 0
-    else: flag = 1
+    # Safe = False | Unsafe = True
+    if d_phi <= threshold * dt_adamba: flag = False
+    else: flag = True
 
     # Reset Env (Set q_pos and q_vel)
     env.sim.set_state(stored_state)
@@ -352,18 +526,60 @@ def check_unsafe(s, point, dt_ratio, dt_adamba, env: gym.Env, threshold):
 
     return flag, env
 
-def out_of_bound(limit, p):
+def check_unsafe(s, point, dt_ratio:float, dt_adamba:float, env:gym.Env, threshold:float,
+                 margin:float, adaptive_k:float, adaptive_n:float, adaptive_sigma:float,
+                 trigger_by_pre_execute:bool, pre_execute_coef:float):
+
+    # Create Action
+    action = point.tolist()
+
+    # Save State of Environment
+    stored_state = copy.deepcopy(env.sim.get_state())
+
+    # Compute Safety Index
+    safe_index_now = env.adaptive_safety_index(k=adaptive_k, sigma=adaptive_sigma, n=adaptive_n)
+
+    # Simulate the Action
+    s_new = env.step(action, simulate_in_adamba=True)
+
+    # Compute Future Safety Index
+    safe_index_future = env.adaptive_safety_index(k=adaptive_k, sigma=adaptive_sigma, n=adaptive_n)
+
+    # Safety Index d_phi = safe_index_future - safe_index_now
+    d_phi = safe_index_future - safe_index_now
+
+    # Pre-Execute Trigger
+    if trigger_by_pre_execute:
+
+        # Safe = False | Unsafe = True
+        if safe_index_future < pre_execute_coef: flag = False
+        else: flag = True
+
+    # Here dt_adamba = dt_env
+    else:
+
+        # Safe = False | Unsafe = True
+        if d_phi <= threshold * dt_adamba: flag = False
+        else: flag = True
+
+    # Reset Env (Set q_pos and q_vel)
+    env.sim.set_state(stored_state)
+    env.sim.forward()
+
+    return flag, env
+
+def out_of_bound(limit, action, sc=False):
 
     # Assert Limit Length
-    assert len(limit[0]) == 2, 'WARNING: Length of Limit != 2'
+    if not sc: assert len(limit[0]) == 2, 'WARNING: Length of Limit != 2'
 
     for i in range(len(limit)):
 
         # Check Limit Make Sense
         assert limit[i][1] > limit[i][0]
 
-        # Limit Violation -> Out-Of-Bound -> Return 1
-        if p[i] < limit[i][0] or p[i] > limit[i][1]: return 1
+        # Limit Violation -> Out-Of-Bound -> Return True
+        if action[i] < limit[i][0] or action[i] > limit[i][1]: return True
 
-    # Return 0 if No-Limit-Violation
-    return 0
+    # Return False if No-Limit-Violation
+    return False
